f40b1594a6f9895528f9d9e6149567926fc1c937
Collins Chirchir <collinskchirchir@gmail.com>
2023-01-18 01:26:13 +0300

    All Done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// Step 1. Import react-router functions
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+
+
+
+function NavBar() {
+  return (
+    <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/login">Login</NavLink>      
+    </nav>
+  );
+}
 
 function Home() {
   return (
@@ -9,8 +23,39 @@ function Home() {
   );
 }
 
-function App() {
-  return <Home />;
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+// Step 2. Change so router is coordinating what is displayed
+ReactDOM.render(
+  <BrowserRouter>
+    <NavBar />
+    <Routes>
+      <Route exact path="/" element={<Home />}></Route>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+    </Routes>
+  </BrowserRouter>, 
+  document.getElementById("root"));
